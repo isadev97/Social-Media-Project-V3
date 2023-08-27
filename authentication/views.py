e0ac598538fc41fce7e8a87565ae003c4b493b68
isadev97 <isa325697@gmail.com>
2023-08-27 20:12:43 +0530

    sign up feature added

--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from authentication.models import User
+from django.contrib.auth.models import auth
+from media_app.models import Profile
 
 # Create your views here.
 def sign_up_view(request):
     page_name = "sign_up.html"
     if request.method == "GET":
+        print(request.user)
+        print(request.user.is_authenticated)
+        print(request.user.username)
         return render(request, page_name)
     else: # POST
-        pass 
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        if email is None:
+            return render(request, page_name, context={"error": True, "error_msg": "Email is required"})
+        if username is None:
+            return render(request, page_name, context={"error": True, "error_msg": "Username is required"})
+        if password is None:
+            return render(request, page_name, context={"error": True, "error_msg": "Password is required"})
+        if User.objects.filter(username=username).exists():
+            return render(request, page_name, context={"error": True, "error_msg": "Username already exists"})
+        if User.objects.filter(email=email).exists():
+            return render(request, page_name, context={"error": True, "error_msg": "Email already exists"})
+        User.objects.create_user(username=username, email=email, password=password)
+        user = auth.authenticate(username=username, password=password)
+        Profile.objects.get_or_create(user=user)
+        auth.login(request, user)
+        return render(request, page_name)
 
 def sign_in_view(request):
-    pass 
+    page_name = "sign_in.html"
+    if request.method == "GET":
+        return render(request, page_name)
+    else: # POST
+        pass
 
 def sign_out_view(request):
     pass 
