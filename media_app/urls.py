31c8577ec785492de67cb53d6900b887b43e80bc
isadev97 <isa325697@gmail.com>
2023-09-02 19:42:50 +0530

    image view added

--- media_app/urls.py
+++ media_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from media_app.views import index_view
+from media_app.views import index_view, submit_post
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('submit-post/', submit_post, name='submit_post'),
 ]
--- media_app/views.py
+++ media_app/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from media_app.models import Post
+
+def get_data(request, key):
+    return request.GET.get(key) or request.POST.get(key)
 
 # Create your views here.
 def index_view(request):
     page_name = "index.html"
     return render(request, page_name)
+
+def submit_post(request):
+    user = request.user
+    content = get_data(request, 'post_caption')
+    image = request.FILES.get('post_image')
+    Post.objects.create(
+        user=user,
+        caption=content,
+        image=image
+    )
+    return redirect('index')
